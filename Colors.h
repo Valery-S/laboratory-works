c5ec15e4a24581342b529ab35e0e135965a2874a
Valery-S <Valerij-01@mail.ru>
2021-03-17 13:18:21 +0300

    Библиотека для переключния цветов в консоли

---
+++ Colors.h
@@ -0,0 +1,10 @@
+#ifndef _Colors_
+#define _Colors_
+const char* Reset = "\u001B[37m";
+const char* Red = "\u001B[31m";
+const char* Green = "\u001B[32m";
+const char* Yellow = "\u001B[33m";
+const char* Dark_Blue = "\u001B[34m";
+const char* Purple = "\u001B[35m";
+const char* Blue = "\u001B[36m";
+#endif
