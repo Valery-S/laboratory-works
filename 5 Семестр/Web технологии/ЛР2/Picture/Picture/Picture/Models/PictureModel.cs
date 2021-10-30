a39e417e3e6940ad381315b28c185b87ec53a2a5
Valery-S <Valerij-01@mail.ru>
2021-10-30 09:26:50 +0300

    Web ЛР2

---
+++ 5 Семестр/Web технологии/ЛР2/Picture/Picture/Picture/Models/PictureModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picture.Models{
+	public class PictureModel{
+		public int Number { get; set; }
+		public int Likes { get; set; }
+		public int Dislike { get; set; }
+
+		public PictureModel(int n, int l, int dl)
+		{
+			Number = n;
+			Likes = l;
+			Dislike = dl;
+		}
+
+	}
+}
---
+++ 5 Семестр/Web технологии/ЛР2/Picture/Picture/Picture/wwwroot/js/Like_or_dislike.js
@@ -0,0 +1,19 @@
+﻿function next_foto() {
+	var rotator = document.getElementById('rotator'),
+		dir = '../Image/50 Foto/',
+		src = rotator.src;
+
+	var num = 0,
+		num_f = Number(src[src.length - 5]),
+		num_s = Number(src[src.length - 6]),
+		len = 50;
+
+	if (isNaN(num_s)) { num = num_f }
+	else { num = num_s * 10 + num_f; }
+
+
+	num++;
+	if (num > len) { num = 1; }
+	rotator.src = dir + num + '.jpg';
+
+}
